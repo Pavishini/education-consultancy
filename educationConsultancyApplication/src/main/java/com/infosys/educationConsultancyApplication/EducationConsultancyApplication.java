be650f975a5f6e285d618ab3f3ef548ad784a883
PAVISHINI <PAVISHINI@Pavishini>
2025-04-10 05:33:52 +0530

    First Commit

---
+++ educationConsultancyApplication/src/main/java/com/infosys/educationConsultancyApplication/EducationConsultancyApplication.java
@@ -0,0 +1,16 @@
+package com.infosys.educationConsultancyApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationConsultancyApplication{
+        public static void main(String[] args)
+        {
+		    SpringApplication.run(EducationConsultancyApplication.class, args);
+	    }
+
+}
+
+//lotus:mango
+//Jasmine:lilylily
---
+++ educationConsultancyApplication/src/test/java/com/infosys/educationConsultancyApplication/EducationConsultancyApplication.java
@@ -0,0 +1,15 @@
+package com.infosys.educationConsultancyApplication;
+
+import org.junit.jupiter.api.Test;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationConsultancyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
